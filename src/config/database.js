71b6da3ebde91080c268121ccf5e252365e81796
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-05-24 13:22:06 -0300

    migratio

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'sistema',
+  define: {
+    timestaps: true,
+    undercored: true,
+    undercoredAll: true,
+
+  }
+
+}
