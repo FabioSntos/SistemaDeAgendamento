8f2f3ba71bbb2f4277a37f3901a7bad923b64765
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-05-27 11:06:02 -0300

    Put Method

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -17,8 +17,26 @@ class UserController {
   }
 
   async update(req, res) {
+    const { email, oldpassword } = req.body;
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+      if (userExists) res.status(400).json({ error: 'Usuário já cadastrado' });
+    }
+
+    if (oldpassword && !(await user.checkPassword(oldpassword))) {
+      return res.status(401).json({ message: 'Senha incorreta' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
     return res.json({
-      message: true,
+      id,
+      name,
+      email,
+      provider,
     });
   }
 }
--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -14,7 +14,7 @@ export default async (req, res, next) => {
 
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-    req.useId = decoded.id;
+    req.userId = decoded.id;
     next();
   } catch (error) {
     return res.status(401).json({
