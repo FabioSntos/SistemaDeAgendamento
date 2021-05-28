58f91281e31bb520b827a5ff184ebc1be31f8613
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-05-28 10:59:07 -0300

    user photo upload

--- src/router.js
+++ src/router.js
@@ -6,7 +6,13 @@ import SessionControler from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
 
+import multer from 'multer';
+
+import multerConfig from './config/multer';
+
 const routes = new Router();
+
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 
@@ -14,7 +20,13 @@ routes.post('/session', SessionControler.store);
 
 //Rotas autenticadas
 routes.use(authMiddleware);
-
 routes.put('/users', UserController.update);
+
+//upload de arquivos
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({
+    message: 'arquivo enviado',
+  });
+});
 
 export default routes;
